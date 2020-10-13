87465902f128e5105619bbc78862a44c2735bd6e
César Alberca <cesalberca@gmail.com>
2020-10-13 19:24:14 +0200

    Hardcode dependencies

--- src/server/local-server.ts
+++ src/server/local-server.ts
@@ -1,7 +1,8 @@
-import { serve } from "../deps.ts";
+import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 
-const s = serve({ port: 8000 });
+const server = serve({ port: 8000 });
 
-for await (const req of s) {
-  req.respond({ body: "Hello World" });
+for await (const request of server) {
+  request.url;
+  request.respond({ body: "Hello World" });
 }
