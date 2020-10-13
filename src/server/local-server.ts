31f3bce730e9a70d504b067478e7bbb5ef6540d9
César Alberca <cesalberca@gmail.com>
2020-10-13 19:35:41 +0200

    Remove request.url

--- src/server/local-server.ts
+++ src/server/local-server.ts
@@ -3,6 +3,5 @@ import { serve } from "https://deno.land/std@0.74.0/http/server.ts";
 const server = serve({ port: 8000 });
 
 for await (const request of server) {
-  request.url;
   request.respond({ body: "Hello World" });
 }
