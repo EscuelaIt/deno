3851738bac291b46c70b8c4d15b8fccccf8172f1
César Alberca <cesalberca@gmail.com>
2020-10-14 18:35:18 +0200

    Add nullish coalescing operator

--- src/randomizer/randomizer.ts
+++ src/randomizer/randomizer.ts
@@ -5,7 +5,7 @@ const server = serve({ port: 8080 });
 for await (const request of server) {
   const [, params] = request.url.split("?");
   const searchParams = new URLSearchParams(params);
-  const start = Number(searchParams.get("start"));
-  const end = Number(searchParams.get("end"));
+  const start = Number(searchParams.get("start") ?? 0);
+  const end = Number(searchParams.get("end") ?? 3);
   request.respond({ body: (Math.random() * end + start).toFixed(0) });
 }
