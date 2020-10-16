579dcc04d8db70a7dd963a0d5d6f2e87e03cf7ca
César Alberca <cesalberca@gmail.com>
2020-10-16 19:41:57 +0200

    Solve second test case using for loop

--- src/fruit-counter/fruit-counter.ts
+++ src/fruit-counter/fruit-counter.ts
@@ -1,3 +1,13 @@
 export function count(elements: string[]) {
-  return { [elements[0]]: elements.length };
+  const countedElements: Record<string, number> = {};
+
+  for (let i = 0; i < elements.length; i++) {
+    if (countedElements[elements[i]] === undefined) {
+      countedElements[elements[i]] = 1;
+    } else {
+      countedElements[elements[i]]++;
+    }
+  }
+
+  return countedElements;
 }
