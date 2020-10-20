56ee293fc2f5153416fc204811e99b07fc1ff46d
César Alberca <cesalberca@gmail.com>
2020-10-20 18:39:27 +0200

    Add frameworks

--- src/flipper/flipper.test.ts
+++ src/flipper/flipper.test.ts
@@ -24,3 +24,47 @@ Deno.test("should flip a word capitalizing correctly", () => {
 
   assertEquals(actual, ["Llohe"]);
 });
+
+Deno.test("should flip all the frameworks", () => {
+  const flipper = new Flipper();
+
+  const actual = flipper.flip([
+    "LitElement",
+    "Angular",
+    "Ember",
+    "React",
+    "Vue",
+    "Stencil",
+    "Preact",
+    "Snowpack",
+    "jQuery",
+    "Svelte",
+    "Express",
+    "Sails",
+    "Koa",
+    "Nuxt",
+    "Next",
+    "Node",
+    "Deno",
+  ]);
+
+  assertEquals(actual, [
+    "Ementlitel",
+    "Ularang",
+    "Berem",
+    "Actre",
+    "Uev",
+    "Ncilste",
+    "Actpre",
+    "Packsnow",
+    "eryjQu",
+    "Ltesve",
+    "Ressexp",
+    "Ilssa",
+    "Oak",
+    "Xtnu",
+    "Xtne",
+    "Deno",
+    "Node",
+  ]);
+});
