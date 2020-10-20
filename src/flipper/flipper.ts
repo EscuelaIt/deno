2187075ffe4c51c8ad8dbc3cb350829d806b5cdc
César Alberca <cesalberca@gmail.com>
2020-10-20 18:29:38 +0200

    Refactor

--- src/flipper/flipper.ts
+++ src/flipper/flipper.ts
@@ -1,18 +1,23 @@
 export class Flipper {
   flip(words: string[]): string[] {
     return words.map((word) => {
-      const end = word.length;
-      const half = end / 2;
+      const half = word.length / 2;
       let firstHalf = word.slice(0, half);
-      let secondHalf = word.slice(half, end);
-
-      if (firstHalf[0].toUpperCase() === firstHalf[0]) {
+      let secondHalf = word.slice(half, word.length);
+      const firstHalfFirstLetter = this.getFirstLetterOfWord(firstHalf);
+      if (firstHalfFirstLetter.toUpperCase() === firstHalfFirstLetter) {
+        const ignoringFirstLetterOfWord = 1;
         secondHalf =
-          secondHalf[0].toUpperCase() + secondHalf.slice(1, secondHalf.length);
+          this.getFirstLetterOfWord(secondHalf).toUpperCase() +
+          secondHalf.slice(ignoringFirstLetterOfWord, secondHalf.length);
         firstHalf = firstHalf.toLowerCase();
       }
 
       return secondHalf + firstHalf;
     });
+  }
+
+  private getFirstLetterOfWord(word: string) {
+    return word[0];
   }
 }
